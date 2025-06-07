f82ddc4c1ba65164f3057f23f12e1877d59213f6
dan13ram <dan13ram@gmail.com>
2025-06-07 12:26:28 +0530

    fixed all eth unit tests

--- eth/util/monitor_test.go
+++ eth/util/monitor_test.go
@@ -7,9 +7,10 @@ import (
 	"time"
 
 	"github.com/dan13ram/wpokt-validator/app"
+	"github.com/dan13ram/wpokt-validator/common"
 	"github.com/dan13ram/wpokt-validator/eth/autogen"
 	"github.com/dan13ram/wpokt-validator/models"
-	"github.com/ethereum/go-ethereum/common"
+	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/assert"
 )
@@ -21,6 +22,8 @@ func TestCreateBurn(t *testing.T) {
 	SENDER_ADDRESS := "0x0000000000000000000000000000000000abcDeF"
 	RECIPIENT_ADDRESS := "0000000000000000000000000000001234567890"
 	ZERO_ADDRESS := "0x0000000000000000000000000000000000000000"
+
+	recipientAddress, _ := common.Bech32FromBytes(app.Config.Pocket.Bech32Prefix, ethcommon.HexToAddress(RECIPIENT_ADDRESS).Bytes())
 
 	testCases := []struct {
 		name            string
@@ -34,7 +37,7 @@ func TestCreateBurn(t *testing.T) {
 			event: &autogen.WrappedPocketBurnAndBridge{
 				Raw: types.Log{
 					BlockNumber: 10,
-					TxHash:      common.HexToHash(TX_HASH),
+					TxHash:      ethcommon.HexToHash(TX_HASH),
 					Index:       0,
 					Address:     common.HexToAddress(ZERO_ADDRESS),
 				},
@@ -43,20 +46,23 @@ func TestCreateBurn(t *testing.T) {
 				Amount:      big.NewInt(100),
 			},
 			expectedBurn: models.Burn{
-				BlockNumber:      "10",
-				Confirmations:    "0",
-				TransactionHash:  TX_HASH,
-				LogIndex:         "0",
-				WPOKTAddress:     ZERO_ADDRESS,
-				SenderAddress:    strings.ToLower(SENDER_ADDRESS),
-				SenderChainID:    app.Config.Ethereum.ChainID,
-				RecipientAddress: strings.ToLower(RECIPIENT_ADDRESS),
-				RecipientChainID: app.Config.Pocket.ChainID,
-				Amount:           "100",
-				CreatedAt:        time.Time{}, // We'll use assert.WithinDuration to check if within an acceptable range
-				UpdatedAt:        time.Time{}, // We'll use assert.WithinDuration to check if within an acceptable range
-				Status:           models.StatusPending,
-				Signers:          []string{},
+				BlockNumber:           "10",
+				Confirmations:         "0",
+				TransactionHash:       TX_HASH,
+				LogIndex:              "0",
+				WPOKTAddress:          ZERO_ADDRESS,
+				SenderAddress:         strings.ToLower(SENDER_ADDRESS),
+				SenderChainID:         app.Config.Ethereum.ChainID,
+				RecipientAddress:      recipientAddress,
+				RecipientChainID:      app.Config.Pocket.ChainID,
+				Amount:                "100",
+				CreatedAt:             time.Time{}, // We'll use assert.WithinDuration to check if within an acceptable range
+				UpdatedAt:             time.Time{}, // We'll use assert.WithinDuration to check if within an acceptable range
+				Status:                models.StatusPending,
+				Signatures:            []models.Signature{},
+				Sequence:              nil,
+				ReturnTransactionHash: "",
+				ReturnTransactionBody: "",
 			},
 			expectedErr:     false,
 			expectedUpdated: 2 * time.Second, // Update time should be within 2 seconds of the current time
