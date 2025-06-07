fd8c371758764b94120a94ac4c810306de8727c1
dan13ram <dan13ram@gmail.com>
2025-06-07 13:03:04 +0530

    fixed coverage script for cross-platform

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+delete_lines_matching() {
+  local pattern="$1"
+  local file="$2"
+
+  if [[ -z "$pattern" || -z "$file" ]]; then
+    echo "Usage: delete_lines_matching <pattern> <file>"
+    return 1
+  fi
+
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "/$pattern/d" "$file"
+  else
+    sed -i "/$pattern/d" "$file"
+  fi
+}
+
 # This script is used to generate coverage report for the project.
 # It will generate a coverage report for the project and open it in the browser.
 
@@ -7,7 +23,13 @@
 go test -cover -coverprofile=coverage.out ./...
 
 # Remove mock files from coverage report
-sed -i '/_mock.go/d' coverage.out
+delete_lines_matching 'mock' coverage.out
+
+# Remove script files from coverage report
+delete_lines_matching 'scripts' coverage.out
+
+# Remove autogen files from coverage report
+delete_lines_matching 'autogen' coverage.out
 
 # Generate HTML coverage report
 go tool cover -html=coverage.out -o coverage.html
